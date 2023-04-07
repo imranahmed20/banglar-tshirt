41208b52d11bcdbff495b7ea904c6045c3e72063
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-04-07 09:57:33 +0600

    first commit

---
+++ src/Component/ReviewOrder/ReviewOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewOrder = () => {
+    return (
+        <div>
+            <h3>This is review order</h3>
+        </div>
+    );
+};
+
+export default ReviewOrder;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Component/Layout/Main';
+import Home from './Component/Home/Home';
+import ReviewOrder from './Component/ReviewOrder/ReviewOrder';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('https://raw.githubusercontent.com/ProgrammingHero1/t-shirt-data/main/tshirt.json')
+      },
+      {
+        path: 'review',
+        element: <ReviewOrder></ReviewOrder>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
